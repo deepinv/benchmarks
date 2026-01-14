adaf943f8012254d7d6a8d59520077e1b4127a42
Julián Tachella <tachellajulian@gmail.com>
2026-01-14 16:47:01 +0100

    add template files

---
+++ benchmark_template/objective.py
@@ -0,0 +1,54 @@
+from benchopt import BaseObjective
+
+import time
+import deepinv as dinv
+from torch.utils.data import DataLoader
+
+
+class Objective(BaseObjective):
+
+    # modify name of the benchmark
+    name = "benchmark_name"
+
+    url = "https://github.com/deep-inverse/benchmarks"
+
+    requirements = ["deepinv"]
+
+    # Minimal version of benchopt required to run this benchmark.
+    # Bump it up if the benchmark depends on a new feature of benchopt.
+    min_benchopt_version = "1.7"
+
+    def set_data(self, dataset, physics):
+        self.dataset = dataset
+        self.physics = physics
+
+    def evaluate_result(self, model):
+        device = getattr(model, 'device', None)
+        self.physics = self.physics.to(device)
+
+        # change metrics if needed
+        metrics = [
+            dinv.loss.PSNR(),
+            dinv.loss.NIQE(device=device)
+        ]
+
+        results = dinv.test(
+            model,
+            DataLoader(self.dataset),
+            self.physics,
+            online_measurements=True,
+            device=device,
+            metrics=metrics,
+            compare_no_learning=False
+        )
+
+        return results
+
+    def get_one_result(self):
+        return dict(model=lambda x: x)
+
+    def get_objective(self):
+        return dict(
+            train_dataset=self.dataset,
+            physics=self.physics,
+        )
---
+++ benchmark_template/solvers/solver1.py
@@ -0,0 +1,30 @@
+from benchopt import BaseSolver
+
+import torch
+import deepinv as dinv
+
+
+class Solver(BaseSolver):
+    name = 'solver_name'
+
+    # add any hyper-parameters here
+    parameters = {}
+
+    sampling_strategy = 'run_once'
+
+    requirements = []
+
+    def set_objective(self, train_dataset=None, physics=None):
+        device = (
+            dinv.utils.get_freer_gpu() if torch.cuda.is_available() else "cpu"
+        )
+
+        # replace by your model, should take (y, physics) as input
+        self.model = dinv.models.RAM(device=device)
+        self.model.device = device
+
+    def run(self, _):
+        pass
+
+    def get_result(self):
+        return dict(model=self.model)
