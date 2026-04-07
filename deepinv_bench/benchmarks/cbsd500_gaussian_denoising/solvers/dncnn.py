3177f4e1c74a41181ccc3ca9853b41414f7fd041
Julián Tachella <tachellajulian@gmail.com>
2026-04-07 13:52:32 +0200

    improve dncnn

--- deepinv_bench/benchmarks/cbsd500_gaussian_denoising/solvers/dncnn.py
+++ deepinv_bench/benchmarks/cbsd500_gaussian_denoising/solvers/dncnn.py
@@ -11,7 +11,20 @@ class Solver(BaseSolver):
 
     def set_objective(self, train_dataset=None, physics=None):
         device = dinv.utils.get_freer_gpu() if torch.cuda.is_available() else "cpu"
-        self.model = dinv.models.ArtifactRemoval(dinv.models.DnCNN(), device=device)
+
+        class RescaledDenoiser(dinv.models.Denoiser):
+            def __init__(self, model, *args, **kwargs):
+                super().__init__(*args, **kwargs)
+                self.base_model = model
+
+            def forward(self, x, sigma):
+                # DnCNN is trained for sigma=2.0/255, so we rescale the input and output
+                sigma_ref = 2.0 / 255
+                factor = sigma_ref / sigma
+                return self.base_model(x * factor) / factor
+
+        rescaled_denoiser = RescaledDenoiser(dinv.models.DnCNN())
+        self.model = dinv.models.ArtifactRemoval(rescaled_denoiser, device=device)
         self.model.device = device
 
     def run(self, _):
